1f8951c91df996c306ab36caab71fe2726a503b5
Jose Arturo Herrera Rivera <joherrerar@unal.edu.co>
2025-07-02 12:41:54 -0500

    v1.0.2

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Repo Scrapper Financiero
 
+## Dependencias
+
+@playwright/test@1.53.2
+├── @types/node@24.0.10
+└── xlsx@0.18.5
+
 ## Versiones
 
 - v0.0.1 -> Commit Inicial
@@ -9,6 +15,9 @@ Repo Scrapper Financiero
 ---
 
 - v1.0.0 -> Scrapper Funcionando, se realizó el cambio del timeout en la configuración para obtener todos los datos de la tabla
+
 - v1.0.1 -> Obtención de encabezados de tabla
+
+- v1.0.2 -> Añadida verificación del símbolo al inicio y espera para obtener datos (10s), xlsx añadido a las dependencias
 
 ---
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,22 @@
 import { test, expect } from "@playwright/test";
+import * as readline from "readline";
 
-const stockSymbol: string[] = ["SPX", "DJI", "IXIC", "RUT"];
+import * as XLSX from "xlsx"; // Importar la librería xlsx
+import path from "path"; // Para manejar rutas de archivos
 
-//console.log("Seleccione el simbolo para el cual desea realizar la prueba:");
+//const stockSymbol: string[] = ["SPX", "DJI", "IXIC", "RUT"];
+let selectedSymbol: string = "DJI";
 
 test("Scrapper", async ({ page }) => {
   await page.goto(
-    `https://finance.yahoo.com/quote/%5E${stockSymbol[0]}/history`,
+    `https://finance.yahoo.com/quote/%5E${selectedSymbol}/history`,
     { waitUntil: "domcontentloaded" }
   );
+
+  //Validar nombre del simbolo
+  console.log(`Navegando a la página de historial de ${selectedSymbol}`);
+  const pageTitle = await page.locator(".yf-4vbjci").first().textContent();
+  console.log(`Historial de ${pageTitle}`);
 
   //Validar que hay tabla
   console.log("Tabla encontrada:", await page.getByRole("table").count());
@@ -16,14 +24,6 @@ test("Scrapper", async ({ page }) => {
 
   // Validar # de filas y columnas en la tabla
   const table = page.getByRole("table");
-
-  // Validar encabezados (DOHLCAV)
-  const headers = table.locator("thead th");
-  console.log("Encabezados de la tabla:");
-  for (let i = 0; i < (await headers.count()); i++) {
-    const headerText = await headers.nth(i).textContent();
-    console.log(`Encabezado ${i + 1}: ${headerText}`);
-  }
 
   const rows = table.locator("tbody tr");
   const rowCount = await rows.count();
@@ -33,10 +33,19 @@ test("Scrapper", async ({ page }) => {
   const columnCount = await columns.count();
   console.log("Número de columnas en la tabla:", columnCount);
 
-  // Scroll the footer into view, forcing an "infinite list" to load more content
-  await page
-    .getByText("Copyright © 2025 Yahoo. All rights reserved.")
-    .scrollIntoViewIfNeeded();
+  // Retraso inicio para obtención de datos
+  console.log(
+    "Esperando 5 segundos para que la tabla se cargue completamente..."
+  );
+  await page.waitForTimeout(5000);
+
+  // Validar encabezados (DOHLCAV)
+  const headers = table.locator("thead th");
+  console.log("Encabezados de la tabla:");
+  for (let i = 0; i < (await headers.count()); i++) {
+    const headerText = await headers.nth(i).textContent();
+    console.log(`Encabezado ${i + 1}: ${headerText}`);
+  }
 
   // obtener datos de cada fila
   for (let i = 0; i < rowCount; i++) {
